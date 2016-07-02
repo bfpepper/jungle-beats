12f6112efed70abae8fba5287d60e529fac67035
Ben Pepper <bfpepper87@gmail.com>
2016-07-01 20:24:22 -0600

    added Rakefile and first test

--- test/jungle_beats_test.rb
+++ test/jungle_beats_test.rb
@@ -1,9 +1,11 @@
 require "minitest/autorun"
-require "minitest/nyay_cat"
+require "minitest/nyan_cat"
 require "./lib/jungle_beats"
 
 class JungleBeatsTest < Minitest::Test
 
-  def test_
+  def test_it_can_create_a_class
+    assert_instance_of JungleBeats, JungleBeats.new
+  end
 
 end
