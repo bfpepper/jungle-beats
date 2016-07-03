bda0e820813effa1b3bab200ff78e41438829765
Ben Pepper <bfpepper87@gmail.com>
2016-07-03 16:44:03 -0600

    built out up to iteration 3 with tests

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,33 +1,86 @@
-require "node"
+require "pry"
+
+require "./lib/node"
 
 class LinkedList
 
   attr_reader :head
 
-  def initialize(head =nil)
-    @head = head
+  def append(sound)
+    if @head == nil
+      @head = Node.new(sound)
+    else
+      @head.next_node = Node.new(sound)
+    end
   end
 
-  def append(sound)
-    #creaete new node
-    #add node to end of current list
-    #expected functionality = list.append("doop") => "doop"
+  def count
+    if @head == nil
+      count = 0
+    else
+      current_node = @head.next_node
+      count = 1
+      until current_node.nil?
+        count += 1
+        current_node = current_node.next_node
+      end
+    end
+    count
+  end
+
+  def to_string
+    string = ""
+    string << @head.data
+    current_node = @head.next_node
+    until current_node == nil
+      string << " " + current_node.data
+      current_node = current_node.next_node
+    end
+    string
+  end
+
+  def prepend(sound)
+    old_head = @head
+    @head = Node.new(sound)
+    @head.next_node = old_head
+  end
+
+  def insert(position, sound)
+    current_node = @head
+    count = 1
+    if count == position
+      old_node = current_node.next_node
+    end
+    until count == position
+      current_node = current_node.next_node
+      count += 1
+      old_node = current_node.next_node
+    end
+    current_node.next_node = Node.new(sound)
+    new_node_3 = current_node.next_node
+    new_node_3.next_node = old_node
+    #rename all *node varialbles
+  end
+
+  def find(start_position, number_of_positions)
+    
   end
 
 end
 
-# > list.append("doop")
-# => "doop"
-
-# > list
-# => <LinkedList head=<Node data="doop" next_node=nil
-      #5678904567890> 45678904567>
-
-# > list.head.next_node
-# => nil
-
-# > list.count
-# => 1
-
 # > list.to_string
-# => "doop"
+# => "deep woo shi shu blop"
+# > list.find(2, 1)
+# => "shi"
+# > list.find(1, 3)
+# => "woo shi shu"
+# > list.includes?("deep")
+# => true
+# > list.includes?("dep")
+# => false
+# > list.pop
+# => "blop"
+# > list.pop
+# => "shu"
+# > list.to_string
+# => "deep woo shi"
--- test/linked_list_test.rb
+++ test/linked_list_test.rb
@@ -9,14 +9,75 @@ class LinkedListTest < Minitest::Test
     assert_instance_of LinkedList, list
   end
 
+  def test_count_is_zero_for_empty_list
+    list = LinkedList.new
+    assert_equal 0 , list.count
+  end
+
   def test_head_is_nil_for_new_instance
     list = LinkedList.new
     assert_nil list.head
   end
 
-  # def test_append_data
-  #   list = LinkedList.new
-  #   
-  # end
+  def test_a_list_is_created_with_a_node
+    list = LinkedList.new
+    list.append("doop")
+    head_node = list.head
+    assert_equal "doop", head_node.data
+  end
+
+  def test_next_node_is_nil
+    list = LinkedList.new
+    list.append("doop")
+    assert_equal nil, list.head.next_node
+  end
+
+  def test_list_can_count
+    list = LinkedList.new
+    list.append("doop")
+    assert_equal 1, list.count
+  end
+
+  def test_sound_is_a_string
+    list = LinkedList.new
+    list.append("doop")
+    head_node = list.head
+    assert_equal "doop", head_node.data
+  end
+
+  def test_it_can_add_multiple_nodes
+    list = LinkedList.new
+    list.append("doop")
+    list.append("deep")
+    assert_equal "deep", list.head.next_node.data
+    assert_equal 2, list.count
+    assert_equal "doop deep", list.to_string
+  end
+
+  def test_it_can_add_a_node_to_the_front
+    list = LinkedList.new
+    list.append("doop")
+    list.append("deep")
+    list.prepend("eeep")
+    assert_equal "eeep", list.head.data
+    assert_equal 3, list.count
+    assert_equal "eeep doop deep", list.to_string
+  end
+
+  def test_it_can_insert_a_sound
+    list = LinkedList.new
+    list.append("doop")
+    list.append("deep")
+    list.insert(1, "woo")
+    assert_equal "doop woo deep", list.to_string
+  end
+
+  def test_it_can_find_a_word
+    list = LinkedList.new
+    list.append("doop")
+    list.append("deep")
+    list.prepend("slop")
+    assert_equal "deep", list.find(2, 1)
+  end
 
 end
